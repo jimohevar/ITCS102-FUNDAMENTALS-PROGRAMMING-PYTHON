ce16174efd49c029ff77c1016581056deb97ce2c
jimohevar <jimohevar@gmail.com>
2025-08-27 10:30:58 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,8 @@
+username = 'Moses'
+password = 'hello'
+
+print(password == 'hello') 
+print(username == 'Moses')
+print((password == 'hello') and (username == 'Mosses'))
+print((password == 'hello') or (username == 'Moses'))
+print(not(password == 'hello') or (username == 'Moses'))
