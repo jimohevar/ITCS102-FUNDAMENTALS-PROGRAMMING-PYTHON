e97523829c864d46dbf3e33564a44b86e80e3ba2
jimohevar <jimohevar@gmail.com>
2025-08-27 11:08:59 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = 'Jimoh'
+password = 'masikip_masarap'
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "Jimoh" and password == "masikip_masarap":
+          print("Access Granted!")
+else:
+          print("Access Denied.")
