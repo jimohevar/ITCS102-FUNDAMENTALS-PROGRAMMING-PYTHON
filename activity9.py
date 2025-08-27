949d0d226d5b3df903ab4abc1f1c4858d8718b7c
jimohevar <jimohevar@gmail.com>
2025-08-27 10:32:58 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,8 @@
+username = 'Jimoh'
+password = 'masikip_masarap'
+
+print(username == 'Jimoh')
+print((username == 'Jimoh')and(password == 'masikip_masarap'))
+print((username == 'Jimoh')or(password == 'masikip_masarap'))
+print(not((username == 'Jimoh')or(password == 'masikip_masarap')))
+
