4340960b658e0a4168e57794af22354d35e6657c
jimohevar <jimohevar@gmail.com>
2025-08-27 10:31:25 +0800

    Delete activity9.py

--- activity9.py
+++
@@ -1,8 +0,0 @@
-username = 'Moses'
-password = 'hello'
-
-print(password == 'hello') 
-print(username == 'Moses')
-print((password == 'hello') and (username == 'Mosses'))
-print((password == 'hello') or (username == 'Moses'))
-print(not(password == 'hello') or (username == 'Moses'))
