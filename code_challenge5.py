b9bbc8831ba46da88917d378506bc2020e86fb81
jimohevar <jimohevar@gmail.com>
2025-09-10 05:58:49 -0400

    Add files via upload

---
+++ code_challenge5.py
@@ -0,0 +1,53 @@
+time = input("Which decade? (2000s, 2010s): ")
+
+if genre == "action":
+   if duration == "long":
+      if time == "2000s" or time == "2000":
+         print("Naruto: ")
+      elif time == "2010s" or "2010":
+         print("Fire Punch: ")
+   if duration == "medium":
+      if time == "2000s" or time == "2000":
+         print("Psyren: ")
+      elif time == "2010s" or "2010":
+         print("Tokyo Ghoul: ")
+   if duration == "short":
+      if time == "2000s" or time == "2000":
+         print("King of Thorn: ")
+      elif time == "2010s" or "2010":
+         print("My Hero Academia: ")
+
+if genre == "romance":
+   if duration == "long":
+      if time == "2000s" or time == "2000":
+         print("Boys Over Flowers: ")
+      elif time == "2010s" or "2010":
+         print("Namaikizikari: ")
+   if duration == "medium":
+      if time == "2000s" or time == "2000":
+         print("Lovely Complex: ")
+      elif time == "2010s" or "2010":
+         print("Ao Haru Ride: ")
+   if duration == "short":
+      if time == "2000s" or time == "2000":
+         print("Kare First Love: ")
+      elif time == "2010s" or "2010":
+         print("Hibi Chouchou: ")
+
+if genre == "horror":
+   if duration == "long":
+      if time == "2000s" or time == "2000":
+         print("Higanjima: ")
+      elif time == "2010s" or "2010":
+         print("Ajin: ")
+   if duration == "medium":
+      if time == "2000s" or time == "2000":
+         print("Ibitsu: ")
+      elif time == "2010s" or "2010":
+         print("Shibito no Kie i Kiku ga Yoi: ")
+   if duration == "short":
+      if time == "2000s" or time == "2000":
+         print("The Drifting Classroom: ")
+      elif time == "2010s" or "2010":
+         print("Hideout: ")
+
