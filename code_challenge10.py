908baeb9623e9e9be188dab9e9c300f9c768310a
Penamante, Jimoh Evar <jimohevar@gmail.com>
2025-09-23 16:42:09 +0800

    Add files via upload

--- code_challenge10.py
+++ code_challenge10.py
@@ -1,8 +1,9 @@
-for i in range(1,11,1):
-    for y in range(10,i,-1):
-        print(" ",end=' ')
-    for z in range(1,i,1):
-       print("*",end=" ")
-    for x in range(1,i,1):
-       print("*",end=" ")
+print("\t\t *",end=" ")
+for i in range(1, 11,1):
+    for y in range(10, i, -1):
+        print(" ", end =' ')
+    for x in range(1, i, 1):
+        print("*", end =' ')
+    for z in range(1, i, 1):
+      print("*", end =' ')
     print()
