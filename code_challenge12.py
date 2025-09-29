18666687c97667bfb9ede11301c0dec4ee932b2b
Penamante, Jimoh Evar <jimohevar@gmail.com>
2025-09-29 11:18:37 +0800

    Add files via upload

---
+++ code_challenge12.py
@@ -0,0 +1,10 @@
+
+# print("\t\t1",end=" ")
+for i in range(1,7,1):
+    for y in range(7,i,-1):
+        print(" ",end=" ") 
+    for x in range(i,0,-1):
+        print(x,end=" ") 
+    for z in range(2, y, 1):
+        print(z,end=" ") 
+    print()
